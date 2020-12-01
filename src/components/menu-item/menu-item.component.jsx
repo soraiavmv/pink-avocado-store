1cb9876b647376709a3b8c86076e8d91639c49f8
soraia <soraia@soraias-Air.Home>
2020-12-01 21:18:28 -0100

    main page selling categories styled and done

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, image, size }) => (
-    <div style={{
-        backgroundImage: `url(${image})`
-    }} className={`${size} menu-item`} >
+    <div className={`${size} menu-item`} >
+        <div style={{
+            backgroundImage: `url(${image})`
+        }} className="background-image"></div>
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">BUY NOW</span>
         </div>
     </div >
--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -10,35 +10,30 @@ class Menu extends React.Component {
             sections: [
                 {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: 'https://i.pinimg.com/564x/e9/4d/63/e94d63db92933dd9bea24c06f2270ded.jpg',
                     id: 1,
-                    linkUrl: 'shop/hats'
                 },
                 {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    title: 'accessories',
+                    imageUrl: 'https://i.pinimg.com/564x/86/d9/83/86d9834ede240350499dafaa48a87463.jpg',
                     id: 2,
-                    linkUrl: 'shop/jackets'
                 },
                 {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    title: 'shoes',
+                    imageUrl: 'https://i.pinimg.com/564x/65/49/80/65498031ea1b596542e17ff74a746c1f.jpg',
                     id: 3,
-                    linkUrl: 'shop/sneakers'
                 },
                 {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    title: 'women',
+                    imageUrl: 'https://i.pinimg.com/564x/b5/60/e4/b560e4b44e9ab938931aaff5b7223c35.jpg',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/womens'
                 },
                 {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    title: 'men',
+                    imageUrl: 'https://i.pinimg.com/564x/7c/28/4b/7c284b29a7f1c491b01a575e8e30f991.jpg',
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/mens'
                 }
             ]
         }
